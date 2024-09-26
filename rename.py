2dc9cc875d3773328ec0dd6fba782e0d8e314133
Sandro Klippel <sandroklippel@gmail.com>
2024-09-26 16:55:22 -0300

    Initial commit

---
+++ rename.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+import shutil
+
+app_name = input("Please enter project name (use - for spaces): ")
+
+FILES = ['setup.py', 'setup.cfg', 'src/project_name/submodule/__init__.py', 'src/project_name/submodule/b.py',
+         'src/project_name/__init__.py', 'src/project_name/utils.py', 'src/project_name/a.py',
+         'src/project_name/main.py']
+FILES = ' '.join(FILES)
+
+os.system("sed -i \"s/project-name/" + app_name + f"/g\" {FILES}")
+os.system("sed -i \"s/project_name/" + app_name.replace("-", "_") + f"/g\" {FILES}")
+shutil.move("src/project_name", f"src/{app_name.replace('-', '_')}")
